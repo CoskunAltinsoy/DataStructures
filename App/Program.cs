48fd9e4c587df62cfb331cea5d4ce283972f30b2
Coşkun Altınsoy <coskun.altinsoyy@gmail.com>
2022-11-08 15:14:32 +0300

    updated

--- App/Program.cs
+++ App/Program.cs
@@ -1,22 +1,40 @@
 ﻿
 using DataStructures.Arrays;
+using System.Collections;
 
-var arr = new Array<int>();
-arr.Add(21);
-arr.Add(32);
-arr.Add(23);
-arr.Add(43);
-arr.Remove();
-arr.Remove();
-arr.Add(43);
-arr.Add(43);
-arr.Add(43);
-arr.Remove();
-arr.Remove();
 
-foreach (var item in arr)
-{
-    Console.WriteLine(item);
-}
+//var arr = new Array<int>(12, 4, 2, 5);
+//var crr = (Array<int>)arr.Clone();
 
-Console.WriteLine($"{arr.Count} / {arr.Capacity}");
+//foreach (var item in arr)
+//{
+//    Console.Write($"{item,-3}");
+//}
+
+//Console.WriteLine();
+
+//foreach (var item in crr)
+//{
+//    Console.Write($"{item,-3}");
+//}
+
+//arr.Add(21);
+//arr.Add(32);
+//arr.Add(23);
+//arr.Add(43);
+//arr.Remove();
+
+
+//foreach (var item in arr)
+//{
+//    Console.WriteLine(item);
+//}
+//Console.WriteLine(" ");
+//Console.WriteLine($"{arr.Count} / {arr.Capacity}");
+
+//var arr = new Array<int>();
+//List<int> list = new List<int>() { 1,3};
+//arr.AddRange(list);
+//arr.ToList().ForEach(x => Console.Write($"{x,-2}"));
+var arr = new Array<int>(1,2,3,4);
+arr.Remove(2);
--- DataStructures/Arrays/Array.cs
+++ DataStructures/Arrays/Array.cs
@@ -12,6 +12,18 @@ namespace DataStructures.Arrays
             InnerList = new T[2];
             Count = 0;
         }
+        public Array(params T[] initial)
+        {
+            InnerList = new T[initial.Length];
+            Count = 0;
+            initial.ToList().ForEach(i => Add(i));
+        }
+        public Array(IEnumerable<T> collection)
+        {
+            InnerList = new T[collection.ToArray().Length];
+            Count = 0;
+            collection.ToList().ForEach(c => Add(c));
+        }
         public void Add(T item)
         {
             if (InnerList.Length==Count)
@@ -21,14 +33,17 @@ namespace DataStructures.Arrays
             InnerList[Count] = item;
             Count++;
         }
-
+        public void AddRange(IEnumerable<T> collection)
+        {
+            collection.ToList().ForEach(c => Add(c));
+        }
         public T Remove()
         {
             if (Count == 0)
             {
                 throw new Exception("There is no item");
             }
-            if (InnerList.Length / 2 == Count) 
+            if (InnerList.Length / 4 == Count) 
             {
                 HalfArray();
             }
@@ -47,7 +62,36 @@ namespace DataStructures.Arrays
             return temp;
 
         }
+        public bool Remove(T item)
+        {
+            if (Count == 0)
+            {
+                throw new Exception("There is no item");
+            }
+            if (InnerList.Length / 4 == Count)
+            {
+                HalfArray();
+            }
+            if (Count > 0)
+            {
+                Count--;
+            }
 
+            var tempArray = new T[Count];
+            int count = 0;
+            for (int i = 0; i < InnerList.Length; i++)
+            {      
+                if (!EqualityComparer<T>.Default.Equals(InnerList[i], item))
+                {
+                    tempArray[count] = InnerList[i];
+                    count++;
+                }
+                
+            }
+            InnerList = tempArray;
+
+            return true;
+        }
         private void HalfArray()
         {
             if (InnerList.Length>2)
@@ -57,7 +101,6 @@ namespace DataStructures.Arrays
                 InnerList = temp;
             }
         }
-
         private void DoubleArray()
         {
             var temp = new T[InnerList.Length * 2];
@@ -66,14 +109,18 @@ namespace DataStructures.Arrays
         }
         public object Clone()
         {
-            throw new NotImplementedException();
+            //return this.MemberwiseClone();// shallow copy
+            var arr = new Array<T>();//deep copy
+            foreach (var item in this)
+            {
+                arr.Add(item);
+            }
+            return arr;
         }
-
         public IEnumerator<T> GetEnumerator()
         {
             return InnerList.Select(x => x).GetEnumerator();
         }
-
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
